dbe3db2b6776790e503aab447281bf9bb319aebc
Aleksei <lex.gmx@gmail.com>
2020-09-01 19:17:52 +0300

    added cicles and conditions

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt("How many films have You watched so far?", "");
 
 let personalMovieDB = {
@@ -8,12 +10,27 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt("What is one of the last films You've seen?", ""),
-      b = prompt("How whould You score it (from 1 to 10)?", ""),
-      c = prompt("What is one of the last films You've seen?", ""),
-      d = prompt("How whould You score it (from 1 to 10)?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("What is one of the last films You've seen?", ""),
+          b = prompt("How whould You score it (from 1 to 10)?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if ((a != null) && (b != null) && (a != '') && (b != '') && (a.length < 50)) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("You've watched not so many movies :(");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a classic movie viewer");
+} else if (personalMovieDB.count > 30) {
+    console.log("You are a movie lover!");
+} else {
+    console.log("Error");
+}
 
 console.log(personalMovieDB);
